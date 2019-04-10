fb283ba35bc05f0d7b98a5d2b94bba905d895c5a
vita2333 <lp_vitadolce@163.com>
2019-04-10 10:14:11 +0800

    :sparkles: add adminLogDesc

--- AdminLogBehavior.php
+++ AdminLogBehavior.php
@@ -39,6 +39,7 @@ class AdminLogBehavior extends Behavior
     public $enableAdminLog = true;
     public $adminLogOnly   = [];
     public $adminLogExcept = [];
+    public $adminLogDesc   = [];
 
     public function events()
     {
@@ -51,9 +52,93 @@ class AdminLogBehavior extends Behavior
     {
         if ($this->isActive($this->owner->action)) {
             Event::on(ActiveRecord::class, BaseActiveRecord::EVENT_AFTER_INSERT, [$this, 'afterModelChanged']);
-            Event::on(ActiveRecord::class, BaseActiveRecord::EVENT_BEFORE_UPDATE, [$this, 'afterModelChanged']);
+            Event::on(ActiveRecord::class, BaseActiveRecord::EVENT_AFTER_UPDATE, [$this, 'afterModelChanged']);
             Event::on(ActiveRecord::class, BaseActiveRecord::EVENT_AFTER_DELETE, [$this, 'afterModelChanged']);
         }
+    }
+
+    public function afterModelChanged($event)
+    {
+        /** @var ActiveRecord $sender */
+        $sender = $event->sender;
+        if ($sender instanceof AdminLog) {
+            return true;
+        }
+
+        if ($event->name === ActiveRecord::EVENT_AFTER_INSERT) {
+            $type = EnumLogType::TYPE_INSERT;
+        } elseif ($event->name === ActiveRecord::EVENT_AFTER_UPDATE) {
+            $type = EnumLogType::TYPE_UPDATE;
+        } elseif ($event->name === ActiveRecord::EVENT_AFTER_DELETE) {
+            $type = EnumLogType::TYPE_DELETE;
+        }
+
+        $info = $this->getUserInfo();
+
+        $tableName = $sender::tableName();
+        $tableName = str_replace(['{{%', '}}'], '', $tableName);
+
+        $log = new AdminLog([
+            'user_id'    => $info[0],
+            'username'   => $info[1],
+            'type'       => $type,
+            'table_name' => $tableName,
+            'route'      => Url::to(),
+            'ip'         => Yii::$app->request->userIP,
+        ]);
+
+        $desc     = $this->getDesc($log,$event);
+
+        $log->description =$desc;
+        $log->save();
+
+        return $log;
+    }
+
+    protected function getUserInfo()
+    {
+        if (Yii::$app->has('user')) {
+            /** @var \yii\web\User $user */
+            $user = Yii::$app->get('user');
+            /** @var \AdminLog\IdentityInterface $identity */
+            if ($identity = $user->getIdentity()) {
+                $uid   = $identity->getId();
+                $uname = $identity->getUsername();
+            } else {
+                $uid   = '0';
+                $uname = '游客';
+            }
+
+            return [$uid, $uname];
+        }
+        throw new InvalidConfigException('user 模块未配置');
+    }
+
+    protected function getDesc($log,$event)
+    {
+        if ($desc = $this->getSpecificDesc($this->owner->action)) {
+            return "{$log->username}{$desc}";
+        }
+        $typeText = EnumLogType::getText($log->type);
+        $changeDesc= $this->getChangedAttrDesc($event);
+        $tablePk  = $event->sender->getPrimaryKey();
+        return  "{$log->username}{$typeText}了表{$log->table_name}:[{$tablePk}]的 {$changeDesc}";
+
+    }
+
+    protected function getChangedAttrDesc($event)
+    {
+        if ( ! empty($event->changedAttributes)) {
+            $desc = '';
+            foreach ($event->changedAttributes as $name => $value) {
+                $desc .= $name . ' : ' . $value . '=>' . $event->sender->getAttribute($name) . ',';
+            }
+            $desc = substr($desc, 0, -1);
+
+            return $desc;
+        }
+
+        return '';
     }
 
     protected function isActive(Action $action): bool
@@ -86,6 +171,21 @@ class AdminLogBehavior extends Behavior
         return ! $exceptMatch && $onlyMatch;
     }
 
+    protected function getSpecificDesc($action)
+    {
+        $id = $this->getActionId($action);
+        foreach ($this->adminLogDesc as $pattern => $desc) {
+            if (StringHelper::matchWildcard($pattern, $id)) {
+                $descMatch = true;
+            }
+        }
+        if ($descMatch) {
+            return $desc;
+        }
+
+        return null;
+    }
+
     protected function getActionId(Action $action)
     {
         if ($this->owner instanceof Module) {
@@ -99,80 +199,6 @@ class AdminLogBehavior extends Behavior
         }
 
         return $id;
-    }
-
-    public function afterModelChanged($event)
-    {
-        /** @var ActiveRecord $sender */
-        $sender = $event->sender;
-        if ($sender instanceof AdminLog) {
-            return true;
-        }
-
-        if ($event->name === ActiveRecord::EVENT_AFTER_INSERT) {
-            $type = EnumLogType::TYPE_INSERT;
-        } elseif ($event->name === ActiveRecord::EVENT_AFTER_UPDATE) {
-            $type = EnumLogType::TYPE_INSERT;
-        } elseif ($event->name === ActiveRecord::EVENT_BEFORE_UPDATE) {
-            $type = EnumLogType::TYPE_INSERT;
-        }
-
-        $info = $this->getUserInfo();
-
-        $tableName = $sender::tableName();
-        $tableName = str_replace(['{{%', '}}'], '', $tableName);
-
-        $log = new AdminLog([
-            'user_id'    => $info[0],
-            'username'   => $info[1],
-            'type'       => $type,
-            'table_name' => $tableName,
-            'route'      => Url::to(),
-            'ip'         => Yii::$app->request->userIP,
-        ]);
-
-        $typeText = EnumLogType::getText($log->type);
-        $desc     = $this->getChangedAttrDesc($event);
-        $tablePk  = $sender->getPrimaryKey();
-
-        $log->description = "{$log->username}{$typeText}了表{$log->table_name}:[{$tablePk}]的 {$desc}";
-        $log->save();
-
-        return $log;
-    }
-
-    protected function getUserInfo()
-    {
-        if (Yii::$app->has('user')) {
-            /** @var \yii\web\User $user */
-            $user = Yii::$app->get('user');
-            /** @var \AdminLog\IdentityInterface $identity */
-            if ($identity = $user->getIdentity()) {
-                $uid   = $identity->getId();
-                $uname = $identity->getUsername();
-            } else {
-                $uid   = '0';
-                $uname = '游客';
-            }
-
-            return [$uid, $uname];
-        }
-        throw new InvalidConfigException('user 模块未配置');
-    }
-
-    protected function getChangedAttrDesc($event)
-    {
-        if ( ! empty($event->changedAttributes)) {
-            $desc = '';
-            foreach ($event->changedAttributes as $name => $value) {
-                $desc .= $name . ' : ' . $value . '=>' . $event->sender->getAttribute($name) . ',';
-            }
-            $desc = substr($desc, 0, -1);
-
-            return $desc;
-        }
-
-        return '';
     }
 
 }
